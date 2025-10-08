e761a3b3e9975d6b50943dd31ed64e939e0dff86
JubesMaci <jubsmaci@gmail.com>
2025-10-08 16:43:35 -0300

    feat: map the structure of routes for simple testing

--- my-app/src/app/home/page.tsx
+++ my-app/src/app/home/page.tsx
@@ -1,11 +1,22 @@
 import React from "react";
+import Link from "next/link";
 
 export default function Home(){
 
     return(
     <>
 
-     <h1>Olá Jubs Dev</h1>
+     <h1>Medi Clinic</h1>
+     <div>
+        <Link href="/doctor/create">Create a new doctor</Link>
+        <br />
+        <Link href="/patient/create">Create a new patient</Link>
+        <br />
+        <Link href="/appointment/create">Create a new appointment</Link>
+        <br />
+        <Link href="/prescription/create">Create a new prescription</Link>
+
+     </div>
 
     </>)
 }
---
+++ my-app/src/app/patient/create/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function PatientCreate (){
+    return(
+        <>
+            <h1>Create Patient Page</h1>
+        </>
+    )
+}
