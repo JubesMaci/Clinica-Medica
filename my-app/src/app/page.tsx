b0f36ccabbc1ea4c84daf219acfec80076ea8d5d
JubesMaci <jubsmaci@gmail.com>
2025-10-10 01:27:29 -0300

    feat: model main pages

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
         password: password,
       };
 
-      const add = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/postDoctor`, {
+      const add = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/login`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
